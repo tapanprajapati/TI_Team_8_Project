71b51c27f29ee683f9dea9528f41a3445f9eda3d
neelkanth <nl543396@dal.ca>
2021-03-24 19:49:30 -0300

    Completed ConfirmedValidator for the password and confirm password validation

--- src/app/signup/confirmed.validator.ts
+++ src/app/signup/confirmed.validator.ts
@@ -6,5 +6,13 @@ export function ConfirmedValidator(controlName: string, matchingControlName: str
   return (formGroup: FormGroup) => {
     const control = formGroup.controls[controlName];
     const matchingControl = formGroup.controls[matchingControlName];
+    if (matchingControl.errors && !matchingControl.errors.confirmedValidator) {
+      return;
+    }
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ confirmedValidator: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
   };
 }
