d8cc6b9728ae219556d017e27cb8016869d8528b
ssamkit108 <ssamkit108@gmail.com>
2021-03-24 00:10:46 -0300

    Developed server component of the cart

---
+++ server/src/routes/cartRoutes.js
@@ -0,0 +1,23 @@
+
+ const express = require('express');
+ const router = express.Router();
+ const { validate } = require('express-validation');
+ 
+ const CartService = require('src/services/CartService');
+ const CartController = require('src/controllers/CartController');
+ const cartController = new CartController(new CartService());
+ const { authenticateRoute } = require('src/helpers/auth');
+ const cartSchema = require('../helpers/validate/cartSchema');
+ 
+ router
+   .route(`/`)
+   .get(authenticateRoute, cartController.getAll)
+   .post(authenticateRoute, validate(cartSchema.addProductToCart), cartController.addProductToCart);
+ 
+ router
+   .route(`/:productId`)
+   .get(authenticateRoute, validate(cartSchema.isProductAvailableInCart), cartController.isProductAvailableInCart)
+   .delete(authenticateRoute, validate(cartSchema.deleteProductFromCart), cartController.deleteProductFromCart);
+ 
+ module.exports = router;
+ 
