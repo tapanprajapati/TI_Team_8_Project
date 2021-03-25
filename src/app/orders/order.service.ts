3210fc5672567899fae8ae6a037f0f5a04ee4250
mjani <janimalav97@gmail.com>
2021-03-25 00:38:47 -0300

    orders module implemented-fetching the data and displaying on table

--- src/app/orders/order.service.ts
+++ src/app/orders/order.service.ts
@@ -1,4 +1,28 @@
+import { environment } from './../../environments/environment.prod';
+import { OrderDetailModel } from './../@core/model/order.model';
+import { Observable } from 'rxjs';
+import { OrderModel } from '@core/model/order.model';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { catchError } from 'rxjs/operators';
+import { GlobalErrorService } from '@core/services/global-error.service';
+import { AuthenticationService } from '@app/auth/authentication.service';
 
 @Injectable({ providedIn: 'root' })
-export class OrderService {}
+export class OrderService {
+  private orders: OrderModel[];
+  private _globalErrorService: GlobalErrorService;
+  private _storage: AngularFireStorage;
+  constructor(private http: HttpClient, private authenticationService: AuthenticationService) {}
+
+  getAllOrders(): Observable<OrderModel[]> {
+    return this.http.get<OrderModel[]>(this._getUrl() + 'user/' + this.authenticationService.authUserEmail);
+  }
+  getOrderDetails(orderId: number) {
+    return this.http.get<OrderDetailModel[]>(this._getUrl() + orderId);
+  }
+  private _getUrl() {
+    return `${environment.serverUrl}orders/`;
+  }
+}
