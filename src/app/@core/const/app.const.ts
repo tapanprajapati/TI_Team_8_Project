c6f3602f5eee5ab7fbc70bae4400e3a78f21498f
ssamkit108 <ssamkit108@gmail.com>
2021-03-26 10:11:52 -0300

    Implemented front for header and footer component

---
+++ src/app/@core/const/app.const.ts
@@ -0,0 +1,25 @@
+const APP_NAME = 'FoodCare';
+
+const APP_TITLES = {
+  home: `Home - ${APP_NAME}`,
+  pickup: `Pickup - ${APP_NAME}`,
+  donate: `Make a Donation - ${APP_NAME}`,
+  products: `Products - ${APP_NAME}`,
+  contact: `Contact Us - ${APP_NAME}`,
+  about: `About Us - ${APP_NAME}`,
+  orders: `Orders - ${APP_NAME}`,
+  profile: `Profile - ${APP_NAME}`,
+  cart: `Cart - ${APP_NAME}`,
+  special_item: `Special Item Request - ${APP_NAME}`,
+  our_team: `Our Team - ${APP_NAME}`,
+  volunteer: `Join As A Volunteer - ${APP_NAME}`,
+  login: `Login - ${APP_NAME}`,
+  signup: `Sign Up - ${APP_NAME}`,
+  forgot_pwd: `Forgot Password - ${APP_NAME}`,
+  update_pwd: `Update Password - ${APP_NAME}`,
+  terms: `Terms & Condition - ${APP_NAME}`,
+  privacy: `Privacy Policy - ${APP_NAME}`,
+  not_found: `Page Not Found - ${APP_NAME}`,
+};
+
+export { APP_NAME, APP_TITLES };
