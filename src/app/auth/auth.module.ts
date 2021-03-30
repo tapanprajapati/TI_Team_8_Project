d578b0799e52af0829254cf2db4088a6e4fd8edb
mjani <janimalav97@gmail.com>
2021-03-30 10:10:17 -0300

    implemented login component with authentication

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { SharedModule } from '@shared';
 import { MaterialModule } from '@app/material.module';
 import { I18nModule } from '@app/i18n';
+
+import { SharedModule } from '@shared';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login.component';
 
